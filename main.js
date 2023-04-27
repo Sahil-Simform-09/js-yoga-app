82cb3b6bf3dc1125511d5e3c41903b8368ed9233
Sahil Mistry <sahil.m@simformsolutions.com>
2023-04-27 10:48:03 +0530

    feat: add scroll functionality to HTML bookmarks

--- main.js
+++ main.js
@@ -16,8 +16,6 @@ navMenu.addEventListener("click", removeMenu);
 document.querySelector(".nav-menu").addEventListener("click", removeMenu);
 
 //carousel functionality
-// Select the carousel you'll need to manipulate and the buttons you'll add events to
-
 const carousel = document.querySelector(".carousel"),
   content = document.querySelector(".content"),
   card = document.querySelector(".card"),
@@ -31,4 +29,24 @@ next.addEventListener("click", e => {
 });
 prev.addEventListener("click", e => {
   carousel.scrollBy(-(card.offsetWidth + 12), 0);
+});
+
+//scroll functinality
+const links = document.querySelectorAll("a");
+links.forEach( link => {
+  link.addEventListener("click", event => {
+
+      //get target div
+      let targetId = link.getAttribute("href");
+      let targetDiv = document.querySelector(targetId);
+
+      //apply scroll functionality
+      targetDiv.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+
+      history.pushState({name: "sahil"}, null, targetId);
+      event.preventDefault();
+  });
 });

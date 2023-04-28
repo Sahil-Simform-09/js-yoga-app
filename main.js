062fe9a436eac67eeaa735fda5128a5b634efea5
Sahil Mistry <sahil.m@simformsolutions.com>
2023-04-28 14:04:45 +0530

    refactor: create functions for different functionality * create doScroll function for scroll functionality * create carousel function for carousel functionality * create createHamburgerMenue function for carousel functionality

--- main.js
+++ main.js
@@ -1,60 +1,76 @@
 // hamburger menu funcionality
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const createHamburgerMenue = function() {
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
 
-const mobileMenu = function () {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}  
-hamburger.addEventListener("click", mobileMenu);
+  const mobileMenu = function () {
+      hamburger.classList.toggle("active");
+      navMenu.classList.toggle("active");
+  }  
+  hamburger.addEventListener("click", mobileMenu);
 
-hamburger.classList.remove("active");
-navMenu.classList.remove("active");
-navMenu.addEventListener("click", removeMenu);
-document.querySelector(".nav-menu").addEventListener("click", removeMenu);
+  const removeMenu = function() {
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active");
+  }
+  navMenu.addEventListener("click", removeMenu);
+  document.querySelector(".nav-menu").addEventListener("click", removeMenu);
+}
 
 // scroll functinality
-const links = document.querySelectorAll("a");
-links.forEach( link => {
-link.addEventListener("click", event => {
+const doScroll = function() {
+  const links = document.querySelectorAll("a");
+  links.forEach( link => {
+  link.addEventListener("click", event => {
 
-    //get target div
-    let targetId = link.getAttribute("href");
-    let targetDiv = document.querySelector(targetId);
+      //get target div
+      let targetId = link.getAttribute("href");
+        let targetDiv = document.querySelector(targetId);
+      //apply scroll functionality
+      targetDiv.scrollIntoView({
+        behavior: "smooth",
+      });
 
-    //apply scroll functionality
-    targetDiv.scrollIntoView({
-      behavior: "smooth",
+      history.pushState(null , null, targetId);
+      event.preventDefault();
     });
-
-    history.pushState(null , null, targetId);
-    event.preventDefault();
   });
-});
+}
 
 // carousel functionality
-const carousel = document.querySelector(".carousel"),
+const carousel = function () {
+  const carousel = document.querySelector(".carousel"),
   card = document.querySelector(".card"),
   next = document.querySelector(".next"),
   prev = document.querySelector(".prev");
 
-let width = card.offsetWidth + 12;
+  let width = card.offsetWidth + 8;
+  next.addEventListener("click", () => {
+    
+    let totalScrollWidth = carousel.offsetWidth < 415 ? width*4 : width*2;
+    
+    console.log(carousel.scrollLeft);
+    console.log(totalScrollWidth);
 
-next.addEventListener("click", e => {
-  carousel.scrollBy(width , 0); 
-  if (carousel.scrollWidth !== 0) {
-    prev.style.display = "flex";
-  }
-  if (carousel.scrollLeft === width*2) {
-    next.style.display = "none";
-  }
-});  
-prev.addEventListener("click", e => {
-    arousel.scrollBy(-width, 0);
-  if (carousel.scrollLeft === width) {
-    prev.style.display = "none";
-  }
-  if (carousel.scrollLeft > 0) {
-    next.style.display = "flex";
-  }
-});
+    carousel.scrollBy(width , 0); 
+    if (carousel.scrollWidth !== 0) {
+      prev.style.display = "flex";
+    }
+    if (carousel.scrollLeft === totalScrollWidth) {
+      next.style.display = "none";
+    }
+  });  
+  prev.addEventListener("click", () => {
+    carousel.scrollBy(-width, 0);
+    if (carousel.scrollLeft === width) {
+      prev.style.display = "none";
+    }
+    if (carousel.scrollLeft > 0) {
+      next.style.display = "flex";
+    }
+  });
+}
+
+doScroll();
+createHamburgerMenue();
+carousel();
